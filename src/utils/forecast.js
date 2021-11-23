a293ac81bf651933399dbafb1025bda90e036845
vahbunaa <anubhavkharel.ak@gmail.com>
2021-11-23 11:18:36 +0545

    added local time

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,4 +1,7 @@
 const request = require('request');
+
+
+
 const forecast = (longitude, latitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=6f431ca9e4557e0a96f590081863167d&query='+ latitude + ',' + longitude;
 
@@ -8,7 +11,7 @@ const forecast = (longitude, latitude, callback) => {
         } else if(body.error) {
             callback('Unable to find location', undefined)
         }else{
-            callback(undefined, body.current.weather_descriptions[0]+ ". It is currently " + body.current.temperature + " degrees out. It feels like " + body.current.feelslike +" degrees out.")
+            callback(undefined, " Local time: " + body.location.localtime + " | " + body.current.weather_descriptions[0]+ ". It is currently " + body.current.temperature + " degrees out. It feels like " + body.current.feelslike +" degrees out.")
         }
     })
 }
